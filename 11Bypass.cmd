9e93739b66481262f61006835ea7d92b27100cdd
Shiro39 <38461122+shirooo39@users.noreply.github.com>
2021-08-03 21:24:22 +0700

    Added BatchGotAdmin
    BatchGotAdmin asks for an elevated access on script launch.
    This way, user don't need to manually run the script as Administrator and they can just forget about it.
    
    This... might be necessary... might be not... who knows...

--- 11Bypass.cmd
+++ 11Bypass.cmd
@@ -1,17 +1,44 @@
 @echo off
-rem ==================================================
+rem ================================================================================
 rem  Windows 11 Requirements Bypass
-rem ==================================================
+rem ================================================================================
 rem
 rem  Written by Shiro39
 rem  Last modified 2021-08-03
 rem
-rem ==================================================
+rem ================================================================================
+title Windows 11
+mode con:cols=80 lines=20
 cls
 
-echo ==================================================
+:: BatchGotAdmin
+:: https://sites.google.com/site/eneerge/scripts/batchgotadmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+echo ================================================================================
 echo  Windows 11 Requirements Bypass
-echo ==================================================
+echo ================================================================================
 echo.
 
 echo Adding key "BypassCPUCheck" into registry...
@@ -31,6 +58,8 @@ echo.
 echo All the needed registries to bypass the requirements has been added.
 echo Now go back to previous page and then go to the next page to proceed.
 echo You should no longer see the "Unsupported" message anymore.
+echo.
+echo ================================================================================
 
 echo.
 echo Press any key to close this CMD...
